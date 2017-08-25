3d97b9d462d1a7fdc6cfa5bbe8fe93a4bd54cd4b
dinesh <31289200+dinuvdm@users.noreply.github.com>
2017-08-25 23:44:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/', function (req, res) {
+app.get('/article-one', function (req, res) {
      res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
-app.get('/', function (req, res) {
+app.get('/article-two', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'articletwo.html'));
 });
-app.get('/', function (req, res) {
+app.get('/article-three', function (req, res) {
     res.sendFile("Article three is requested here");
 });
 app.get('/ui/style.css', function (req, res) {
